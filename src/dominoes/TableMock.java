437894cd130da4a92227b525c1cf786f07230552
Sophie Woods <sophiecwoods1@gmail.com>
2022-04-06 19:45:28 +0100

    Add tests for makePlay method. Create TableMock subclass of Table to access _layout field.

---
+++ src/dominoes/TableMock.java
@@ -0,0 +1,23 @@
+package dominoes;
+
+import java.util.Vector;
+
+public class TableMock extends Table {
+
+    Vector _layout;
+
+    public TableMock(Vector layout) {
+        _layout = layout;
+    }
+
+    @Override
+    public int left() {
+        return ((Bone)this._layout.firstElement()).left();
+    }
+
+    @Override
+    public int right() {
+        return ((Bone)this._layout.lastElement()).right();
+    }
+
+}
